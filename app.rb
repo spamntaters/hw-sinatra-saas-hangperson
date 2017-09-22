ff2ef8c862d0f980aaea8118cec364e0be5b1ad5
spamntaters <smurra28@uncc.edu>
2017-09-21 20:33:05 -0400

    Completed part 5
    Signed-off-by: spamntaters <smurra28@uncc.edu>

--- app.rb
+++ app.rb
@@ -51,17 +51,28 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+      when :win
+        redirect '/win'
+      when :lose
+        redirect '/lose'
+      when :play
+        erb :show# You may change/remove this line
+    end
   end
 
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    end
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    end
   end
 
 end
